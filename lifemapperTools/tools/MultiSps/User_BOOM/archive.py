9d3760b54b58207cff2d192803fccac3afcfbab5
jcavner <jcavner@ku.edu>
2016-10-10 16:53:02 -0500

    icon view, context menu, add new, selection and edit

--- lifemapperTools/tools/MultiSps/User_BOOM/archive.py
+++ lifemapperTools/tools/MultiSps/User_BOOM/archive.py
@@ -5,6 +5,8 @@ from qgis.gui import *
 from PyQt4.Qt import * 
 from lifemapperTools.common.lmHint import Hint, SpeciesSearchResult
 from lifemapperTools.common.lmListModel import LmListModel
+from lifemapperTools.icons import icons
+from __builtin__ import True
 
 
 class ArchiveComboModel(LmListModel):
@@ -47,6 +49,56 @@ class ArchiveComboModel(LmListModel):
       self.listData = newList
       self.endInsertRows()
 
+class PAMIconListModel(LmListModel):
+   
+   def data(self, index, role):
+      """
+      @summary: Gets data at the selected index
+      @param index: The index to return
+      @param role: The role of the item
+      @return: The requested item
+      @rtype: QtCore.QVariant
+      """
+      if not index.isValid() or not (0 <= index.row() < len(self.listData)):
+         return None #QVariant()
+      if role == Qt.DisplayRole:
+         return self.listData[index.row()][1]
+      if role == Qt.DecorationRole:
+         return self.listData[index.row()][0]
+      return None #QVariant()
+   
+   def flags(self, index):   
+      if True: #index.column() in self.editIndexes and index.column() not in self.controlIndexes:     
+         return Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsEditable  
+      #elif index.column() in self.controlIndexes:
+      #   return Qt.ItemIsEnabled | Qt.ItemIsSelectable   
+        
+      return QAbstractTableModel.flags(self, index)
+   
+   def setData(self, index, value, role=Qt.EditRole):
+      """
+      @summary: sets an individual item (cell) in the table data
+      """
+      try:
+         self.listData[index.row()][1] = value.toString()
+      except Exception, e:
+         self.listData[index.row()][1] = str(value)
+      self.dataChanged.emit(index,index)
+      return True
+   
+   def updateList(self, newList):
+      """
+      @summary: Updates the contents of the list
+      @param newList: A list of items to use for the new list
+      @note: The provided list will replace the old list 
+      """
+      self.beginRemoveRows(QModelIndex(),0,len(self.listData)) # optional
+      self.listData = [] # optional
+      self.endRemoveRows() # optional
+      self.beginInsertRows(QModelIndex(), 0, len(newList)) #just len makes auto with setIndex work better
+      self.listData = newList
+      
+      self.endInsertRows()
 
 class SpsSearchResult(object):
    
@@ -121,7 +173,10 @@ class ArchiveHint(Hint):
 
 class Search(object):
    
-   
+   #def __init__(self):
+      
+   #   self.PAMProjectView()
+      
    def hintBox(self, data=[]):
       
       """
@@ -149,6 +204,8 @@ class Search(object):
          for res in self.taxa[str(currentItem)]:
             QListWidgetItem(res, self.listView, QListWidgetItem.UserType)
    
+  
+      
    def archiveListView(self):
       
       self.listView = QListWidget()
@@ -161,8 +218,88 @@ class Search(object):
       self.taxa = taxa
       spsByTaxa = [SpsSearchResult(k,taxa[k]) for k in taxa.keys()]
       return spsByTaxa
+
+class PAMList():
    
-class UserArchiveController(Search):
+   def PAMProjectView(self):
+      
+      self.projectCanvas = QListView()
+      self.projectCanvas.setViewMode(QListView.IconMode)
+      self.projectCanvas.setMovement(QListView.Static)
+      self.projectCanvas.setGridSize(QSize(100,100))
+      self.projectCanvas.setIconSize(QSize(80,80))
+      self.projectCanvas.setWrapping(True)
+      
+      self.projectCanvas.setContextMenuPolicy(Qt.CustomContextMenu)
+      self.projectCanvas.customContextMenuRequested.connect(self.setAllPopUp)
+      
+   def setAllPopUp(self,pos):
+      try:
+         index = self.projectCanvas.indexAt(pos)
+         value = index.model().listData[index.row()][index.column()]
+         menu = QMenu()
+         setAllAction = menu.addAction('Set All in this column to "'+str(value)+'"')
+         setSelected = menu.addAction('Set Selected in this column to "'+str(value)+'"')
+         setEmpty = menu.addAction('Set empty cells in this column to "'+str(value)+'"')
+         action = menu.exec_(QCursor.pos())
+         if action == setAllAction:
+            self.setAllInColumn(index)
+         elif action == setSelected:
+            self.setColumnsInSelectedRows(index)
+         elif action == setEmpty:
+            self.setEmptyInColumn(index)
+      except Exception, e:
+         print str(e)  
+      
+   def newButton(self,newButController=None):
+      
+      newPAMButton = QPushButton("New PAM")
+      if newButController is not None:
+         newPAMButton.clicked.connect(newButController)
+      newPAMButton.setAutoDefault(False)
+      newPAMButton.setMinimumWidth(158)   # MIGHT WANT TO RETURN HORIZ LAYOUT
+      return newPAMButton
    
-   pass
+class UserArchiveController(Search,PAMList):
+   
+   def __init__(self):
+      
+      self.PAMProjectView()
+      self.getData()
+      
+   def getData(self):
+      self.data = []
+      try:
+         for i,x in enumerate(range(0,25)):
+            icon = QIcon(":/plugins/lifemapperTools/icons/Grid_Owl.png")
+            self.data.append([icon,"PAM %s" % (int(i))])
+      except:
+         pass
+      else:
+         self.archiveModel = PAMIconListModel(self.data)
+         self.projectCanvas.setModel(self.archiveModel)
+         
+   def createNewPAM(self):
+      
+      self.projectCanvas.setEditTriggers(QAbstractItemView.CurrentChanged)
+      icon = QIcon(":/plugins/lifemapperTools/icons/Grid_Owl.png")
+      self.data.insert(0, [icon,"New PAM"])
+      self.archiveModel.updateList(self.data)
+      
+      self.projectCanvas.setEditTriggers(QAbstractItemView.CurrentChanged)
+      newIdx = self.archiveModel.index(0)
+      #selModel = self.projectCanvas.selectionModel()
+      #selModel.select(newIdx,QItemSelectionModel.Select)
+      self.projectCanvas.setCurrentIndex(newIdx)
+      self.projectCanvas.setEditTriggers(QAbstractItemView.DoubleClicked)
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
       
