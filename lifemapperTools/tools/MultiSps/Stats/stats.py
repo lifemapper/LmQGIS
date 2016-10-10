8a0a7fa6c01f6f925cc6e4805ec2f2edfda5e27c
jcavner <jcavner@ku.edu>
2016-10-10 16:54:26 -0500

    stat skeleton

---
+++ lifemapperTools/tools/MultiSps/Stats/stats.py
@@ -0,0 +1,15 @@
+#from qgis.core import *
+#from qgis.gui import *
+#from PyQt4.Qt import * 
+from lifemapperTools.common.lmListModel import LmListModel
+
+class Stats_UI(object):
+
+   def listView(self):
+      #self.statsList = QListView()
+      pass
+
+class StatsController(Stats_UI):
+   
+   def populateStatsList(self):
+      pass
